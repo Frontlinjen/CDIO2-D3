1cfdac3b16cdbd25b8afc0a09c025845779015da
Thomas Bech Madsen <thomasbm@live.dk>
2016-04-25 15:55:57 +0200

    CDIO3 Beginning

---
+++ src/webInterface/client/CDIO3_D2_WebInterface.java
@@ -0,0 +1,60 @@
+package webInterface.client;
+
+import webInterface.shared.FieldVerifier;
+
+import com.gargoylesoftware.htmlunit.javascript.host.Text;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class CDIO3_D2_WebInterface implements EntryPoint {
+	/**
+	 * The message displayed to the user when the server cannot be reached or
+	 * returns an error.
+	 */
+	private static final String SERVER_ERROR = "An error occurred while "
+			+ "attempting to contact the server. Please check your network " + "connection and try again.";
+
+	/**
+	 * Create a remote service proxy to talk to the server-side Greeting service.
+	 */
+	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() 
+	{
+		RootPanel container = RootPanel.get("operatorList");
+		String[] names = {
+				"Senads søster",
+				"Senads mor", 
+				"Rallemuller",
+				"Isbjørn",
+				"Sugardaddy", 
+				"Horsepumping",
+				"Thomas",
+				"Shiniqua"
+		};
+		for(String s : names)
+		{
+			Label t = new Label(s);
+			container.add(t);
+		}
+	}
+}
