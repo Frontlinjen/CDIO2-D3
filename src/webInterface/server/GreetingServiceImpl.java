4257c939402e996c993a143ed5bd2baa8b1bb6b4
Hjorthen <Hjorthensebastian@gmail.com>
2016-04-25 23:10:03 +0200

    Fixed compilition error

--- src/webInterface/server/GreetingServiceImpl.java
+++ src/webInterface/server/GreetingServiceImpl.java
@@ -11,7 +11,7 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 public class GreetingServiceImpl extends RemoteServiceServlet implements GreetingService {
 
 	public String greetServer(String input) throws IllegalArgumentException {
-		
+		return input;
 	}
 
 	/**
