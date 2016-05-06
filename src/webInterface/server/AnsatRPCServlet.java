2c0bdfa57265fd4c9ae4613d8df34b5b11c1efc7
bøllemis <Bruger@10.16.74.103>
2016-05-06 09:33:53 +0200

    Added logic to AnsatRPCServlet

--- src/webInterface/server/AnsatRPCServlet.java
+++ src/webInterface/server/AnsatRPCServlet.java
@@ -5,37 +5,64 @@ import java.util.List;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import webInterface.client.AnsatRPCInterface;
+import webInterface.server.Database.DALException;
+import webInterface.server.Database.MySQLAnsatDAO;
 import webInterface.shared.AnsatDTO;
 
 public class AnsatRPCServlet extends RemoteServiceServlet implements AnsatRPCInterface {
 
+	MySQLAnsatDAO database = new MySQLAnsatDAO();
+	
+	
 	@Override
 	public AnsatDTO getAnsat(String cpr) {
-		// TODO Auto-generated method stub
+		try {
+			return database.getAnsat(cpr);
+		} catch (DALException e) {
+			System.out.println("Failed at getAnsat");
+		}
 		return null;
 	}
 
 	@Override
 	public List<AnsatDTO> getAnsatList() {
-		// TODO Auto-generated method stub
+		try{
+			return database.getAnsatList();
+		} catch (DALException e){
+			System.out.println("Failed at getAnsatList");
+		}
 		return null;
 	}
 
 	@Override
-	public void createAnsat(AnsatDTO ans) {
-		// TODO Auto-generated method stub
+	public int createAnsat(AnsatDTO ans) {
+		try {
+		return database.createAnsat(ans);
+		} catch (DALException e){
+			System.out.println("Failed at createAnsat");
+		}
+		return 0;
+	}
+
+	@Override
+	public int updateAnsat(AnsatDTO ans) {
+		try {
+			return database.updateAnsat(ans);
+			} catch (DALException e){
+				System.out.println("Failed at updateAnsat");
+			}
+			return 0;
 		
 	}
 
 	@Override
-	public void updateAnsat(AnsatDTO ans) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteAnsat(AnsatDTO ans) {
-		// TODO Auto-generated method stub
+	public int deleteAnsat(AnsatDTO ans) {
+		try {
+			return database.deleteAnsat(ans);
+			} catch (DALException e){
+				System.out.println("Failed at deleteAnsat");
+			}
+			return 0;
 		
 	}
 	
